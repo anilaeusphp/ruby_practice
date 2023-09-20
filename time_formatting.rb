7163f9d6e6bcd578059abdcbabb8a2643f0f1db1
rubyongainz <rubyongainz@gmail.com>
2023-09-20 14:42:41 +0300

    some timing functions has been added

--- time_formatting.rb
+++ time_formatting.rb
@@ -1,2 +1,15 @@
 puts Time.now.strftime("%H:%M")
 # 14:39    Hour and Minute
+
+
+puts Time.now.day  # 20     what it means that which number of day in the current month
+
+puts Time.now.hour # 14
+
+puts Time.now.min # 42
+
+puts Time.now.sec # 2
+
+puts Time.now.year  # 2023
+
+puts Time.now.month  # 9    , number of the current month
